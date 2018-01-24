3e643291a7baff110a4b4c7a57d5f805630df560
msitkowski <mateuszsitek.sitkowski@gmail.com>
2018-01-24 21:42:14 +0100

    init

---
+++ WebApplication/WebForm.aspx
@@ -0,0 +1,31 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm.aspx.cs" Inherits="WebApplication.WebForm" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+        </div>
+        <div>
+            <asp:Label ID="Label1" runat="server" Text="Matrix1"></asp:Label>
+            <asp:FileUpload ID="FileUpload1" runat="server" />
+        </div>
+        <div>
+            <asp:Label ID="Label2" runat="server" Text="Matrix2"></asp:Label>
+            <asp:FileUpload ID="FileUpload2" runat="server" />
+        </div>
+        <div>
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Pomnóż macierze" />
+        </div>
+        <div>
+            <asp:Label ID="Label3" runat="server" Text="Results"></asp:Label><asp:Label ID="Label4" runat="server" Text=""></asp:Label>
+            <asp:Button ID="saveButton" runat="server" Text="Save results" Enabled="False" OnClick="saveButton_Click1" />
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication/WebForm.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace WebApplication
+{
+    public partial class WebForm : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            string path = "D:\\STUDIA\\programy\\WebApplication\\WebApplication\\Data\\";
+            FileUpload1.PostedFile.SaveAs(path + "m1.txt");
+            FileUpload2.PostedFile.SaveAs(path + "m2.txt");
+
+            Matrix matrix1 = new Matrix("m1.txt");
+            Matrix matrix2 = new Matrix("m2.txt");
+
+            Matrix res = new Matrix();
+            res = res.Multiply(matrix1, matrix2);
+            string filename = res.saveInFile();
+            Label4.Text = filename;
+            saveButton.Enabled = true;
+        }
+
+        protected void saveButton_Click1(object sender, EventArgs e)
+        {
+            string fileName = Label4.Text;
+            Response.ContentType = "text/plain";
+            Response.AppendHeader("Content-Disposition", "attachment; filename=" + fileName);
+            Response.TransmitFile(Server.MapPath("~/Data/" + fileName));
+            Response.End(); 
+        }
+    }
+}
